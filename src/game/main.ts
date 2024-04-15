cf6ad17e03adfd998791cf6fb5495e5bc4f5973c
Artem <artemzi1000@gmail.com>
2024-04-15 12:49:01 +0700

    Подгтовка сцены

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,31 +1,22 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
+import { Boot } from './scenes/Boot';
+import { Game as MainGame } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+    type: AUTO, width: '100%', height: '100%', parent: 'game-container', backgroundColor: '#028af8', physics: {
+        default: 'matter', matter: { debug: true, gravity: { x: 0, y: 100 } },
+    }, scene: [Boot, Preloader, MainGame, MainMenu, GameOver],
 };
 
 const StartGame = (parent: string) => {
 
     return new Game({ ...config, parent });
 
-}
+};
 
 export default StartGame;
--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -2,75 +2,34 @@ import { GameObjects, Scene } from 'phaser';
 
 import { EventBus } from '../EventBus';
 
-export class MainMenu extends Scene
-{
+export class MainMenu extends Scene {
     background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
-    logoTween: Phaser.Tweens.Tween | null;
 
-    constructor ()
-    {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+    create() {
+        const width = this.game.canvas.width;
+        const height = this.game.canvas.height;
+        console.log({ width, height });
+        this.matter.world.setBounds(0, 0, width, height);
 
-        this.logo = this.add.image(512, 300, 'logo').setDepth(100);
+        const x = Phaser.Math.Between(0, width);
+        const y = Phaser.Math.Between(0, height);
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        if (Math.random() < 0.7) {
+            const sides = Phaser.Math.Between(3, 14);
+            const radius = Phaser.Math.Between(8, 50);
+
+            this.matter.add.polygon(x, y, sides, radius, { restitution: 0.9 });
+        } else {
+            const width = Phaser.Math.Between(16, 128);
+            const height = Phaser.Math.Between(8, 64);
+
+            this.matter.add.rectangle(x, y, width, height, { restitution: 0.9 });
+        }
 
         EventBus.emit('current-scene-ready', this);
-    }
-    
-    changeScene ()
-    {
-        if (this.logoTween)
-        {
-            this.logoTween.stop();
-            this.logoTween = null;
-        }
-
-        this.scene.start('Game');
-    }
-
-    moveLogo (vueCallback: ({ x, y }: { x: number, y: number }) => void)
-    {
-        if (this.logoTween)
-        {
-            if (this.logoTween.isPlaying())
-            {
-                this.logoTween.pause();
-            }
-            else
-            {
-                this.logoTween.play();
-            }
-        } 
-        else
-        {
-            this.logoTween = this.tweens.add({
-                targets: this.logo,
-                x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
-                y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
-                yoyo: true,
-                repeat: -1,
-                onUpdate: () => {
-                    if (vueCallback)
-                    {
-                        vueCallback({
-                            x: Math.floor(this.logo.x),
-                            y: Math.floor(this.logo.y)
-                        });
-                    }
-                }
-            });
-        }
     }
 }
