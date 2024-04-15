b67dddcba9ee7cb390087af46e62ec8e95d35ead
Artem <artemzi1000@gmail.com>
2024-04-15 13:39:59 +0700

    Два шара

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,7 +1,5 @@
 import { AUTO, Game } from 'phaser';
 import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
@@ -9,8 +7,8 @@ import { Preloader } from './scenes/Preloader';
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO, width: '100%', height: '100%', parent: 'game-container', backgroundColor: '#028af8', physics: {
-        default: 'matter', matter: { debug: true, gravity: { x: 0, y: 100 } },
-    }, scene: [Boot, Preloader, MainGame, MainMenu, GameOver],
+        default: 'matter', matter: { debug: true, gravity: { x: 0, y: 1 } },
+    }, scene: [Boot, Preloader, MainMenu],
 };
 
 const StartGame = (parent: string) => {
--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -15,20 +15,8 @@ export class MainMenu extends Scene {
         console.log({ width, height });
         this.matter.world.setBounds(0, 0, width, height);
 
-        const x = Phaser.Math.Between(0, width);
-        const y = Phaser.Math.Between(0, height);
-
-        if (Math.random() < 0.7) {
-            const sides = Phaser.Math.Between(3, 14);
-            const radius = Phaser.Math.Between(8, 50);
-
-            this.matter.add.polygon(x, y, sides, radius, { restitution: 0.9 });
-        } else {
-            const width = Phaser.Math.Between(16, 128);
-            const height = Phaser.Math.Between(8, 64);
-
-            this.matter.add.rectangle(x, y, width, height, { restitution: 0.9 });
-        }
+        this.matter.add.circle(250, height - 100, 50);
+        this.matter.add.circle(200, height - 500, 50);
 
         EventBus.emit('current-scene-ready', this);
     }
